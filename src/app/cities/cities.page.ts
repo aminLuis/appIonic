91ae3fb6eb75250f663b5e839e55f68fc6d1d6b5
aminLuis <aminluisamin@gmail.com>
2023-01-01 21:53:40 -0500

    Alerts - Toast y Alert

--- src/app/cities/cities.page.ts
+++ src/app/cities/cities.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CitiesServiceService } from '../services/cities-service.service';
 import { Citie } from '../interfaces/citie.interface';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cities',
@@ -11,7 +12,10 @@ export class CitiesPage implements OnInit {
 
   cities: Citie[];
 
-  constructor(private cities_service:CitiesServiceService) { }
+  constructor(private cities_service:CitiesServiceService,
+    public toastController: ToastController,
+    public alertController: AlertController
+    ) { }
 
   ngOnInit() {
     this.list_cities();
@@ -22,6 +26,52 @@ export class CitiesPage implements OnInit {
       this.cities = res.data;
       console.log(this.cities);
     });
+  }
+
+  async presentToast1(){
+    const toast = this.toastController.create({
+      message: 'Ciudad seleccionada',
+      duration: 2000,
+      position: 'bottom'
+    });
+    (await toast).present();
+  }
+
+  async presentAlert1(){
+    const alert = this.alertController.create({
+      header: 'Borrar ciudad',
+      message: 'Se ha borrado la ciudad',
+      buttons: ['OK']
+    });
+    (await alert).present();
+
+    let result = (await alert).onDidDismiss();
+    console.log(result);
+  }
+
+  async presentAlert2(){
+    const alert = this.alertController.create({
+      header: 'Borrar ciudad',
+      message: '¿Está seguro que desea eliminar la ciudad?',
+      buttons: [
+        {
+          text: 'No',
+          handler: ()=>{
+            console.log('NO CANCEL');
+          }
+        },
+        {
+          text: 'Si',
+          handler: ()=>{
+            console.log('Ciudad eliminada');
+          }
+        }
+      ]
+    });
+    (await alert).present();
+
+    let result = (await alert).onDidDismiss();
+    console.log(result);
   }
 
 }
